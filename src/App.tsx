3ecf67507c073ce2ef327580f7f21c9441ebcb6f
Annie Fasching <anna.fasching@hotmail.com>
2023-07-11 11:04:01 +0200

    first try

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {MouseEventHandler, useState } from 'react';
+import './App.css'
+
+interface CounterProps {
+  count?: number;
+  inc?: MouseEventHandler;
+}
+
+const CountDisplay = ({ count }: CounterProps) => <div>{count}</div>
+
+const Button = ({ inc }: CounterProps) => <button className='myButton' onClick={inc}></button>
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const inc = () => setCounter(counter + 1);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CountDisplay count={counter}></CountDisplay>
+      <Button inc={inc}></Button>
+      <button className="myButton" style={counter < 10 ? { background: "blue" } : { background: "red" }}>
+      </button>
+    </>
   );
 }
 
